acd52e13fc9703f34f29f5953a4bbdff477b75b7
Sergey <sergeye.kravchenko@gmail.com>
2018-02-20 14:10:46 +0300

    add using external source app.properties

--- src/main/java/com/kravchenko/designermanager/DesignermanagerApplication.java
+++ src/main/java/com/kravchenko/designermanager/DesignermanagerApplication.java
@@ -4,9 +4,11 @@ import com.kravchenko.designermanager.processor.OrderedItemProcessor;
 import com.kravchenko.designermanager.security.UrlConnector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class DesignermanagerApplication {
@@ -18,8 +20,13 @@ public class DesignermanagerApplication {
     private UrlConnector connector;
 
     public static void main(String[] args) {
-        SpringApplication.run(
-                DesignermanagerApplication.class, args);
+        String prop = System.getProperty("app.conf");
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(DesignermanagerApplication.class);
+
+        if (prop != null && !prop.equals("") && new File(prop).exists()) {
+            builder.properties("spring.config.location=" + prop);
+        }
+        builder.run(args);
     }
 
     @Bean
